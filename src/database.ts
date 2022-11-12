75e1246f70ff17c1fdad6c58b876c77a2a6c6c83
Sergio <sergiomendes23@gmail.com>
2022-11-11 21:57:24 -0300

    database connected

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,13 @@
 import pg from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const { Pool } = pg;
+
 const connection = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: '19081992',
+    database: 'palpitexa'
   });
 
 export default connection;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
 import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
 
 const server = express();
-server.use(cors());
 server.use(express.json());
-dotenv.config();
 
-const PORT = process.env.PORT;
+
+const PORT = 4000;
 server.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
